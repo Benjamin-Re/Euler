73138e5128c6e0a36daf012965371a9fdf1c051f
Benjamin-Re <renker.benjamin@gmail.com>
2016-08-20 12:26:26 +0200

    Add files via upload

--- Euler3.js
+++ Euler3.js
@@ -1,3 +1,5 @@
+// This is actually ReebootJeff's solution. I had no idea what to do here and his solution helped me understand it. So thanks to Jeff. 
+
 var isPrime = function (input) {
 	for (var i=2; i < input; i++){
 		if (input % i === 0) {
